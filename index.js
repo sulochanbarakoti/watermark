5754e5db3564b42fb65e2c91691b1536ab97b958
sulochanbarakoti <sulochanbarakoti@gmail.com>
2019-05-23 17:22:50 +0545

    second

--- index.js
+++ index.js
@@ -4,34 +4,54 @@ var bodyParser = require('body-parser');
 var port = process.env.PORT || 8080;
 var path = require('path');
 var watermark = require('image-watermark');
-app.use(express.static(path.join(__dirname, 'public')));
+var Jimp = require('jimp');
 
-const which = require('which');
-const spawn = require('child_process').spawn;
-// Find npm in PATH
-const npm = which.sync('npm');
-// Execute
-const noErrorSpawn = spawn(npm, ['install']);
+
+let imgRaw = './public/src/chitwan.jpg'; //a 1024px x 1024px backgroound image
+let imgLogo = './public/img/logo.png'; //a 155px x 72px logo
+let imgExported = './public/src/image1.jpg';
+let textData = {
+  text: '© JKRB Investments Limited', //the text to be rendered on the image
+  maxWidth: 1004, //image width - 10px margin left - 10px margin right
+  maxHeight: 72 + 20, //logo height + margin
+  placementX: 10, // 10px in on the x axis
+  placementY: 1024 - (72 + 20) - 10 //bottom of the image: height - maxHeight - margin 
+};
 
 
 app.get('/watermark', function (req, res) {
-  var fs = require('fs');
-  var options = {
-    'text': 'sample watermark',
-    // 'dstPath' : 'watermark.jpg',
-  };
-  const imagePath = path.resolve(__dirname, './public/src/chitwan.jpg');
-  console.log(imagePath);
-  if (fs.existsSync(imagePath)) {
-    watermark.embedWatermark('./public/src/chitwan.jpg', options);
-    // res.send("done")
-    // return
-    res.send('Hello world');
-  } else {
-    res.json({ "filesexist": "no" });
-  }
-});
+  Jimp.read(imgRaw)
+    .then(tpl =>
+      Jimp.read(imgLogo).then(logoTpl => {
+        logoTpl.opacity(0.4);
+        return tpl.composite(logoTpl,
+          Jimp.AUTO,
+          Jimp.AUTO,
+          Jimp.HORIZONTAL_ALIGN_CENTER
+          | Jimp.VERTICAL_ALIGN_MIDDLE);
+      }))
+    .then(tpl => (
+      Jimp.loadFont(Jimp.FONT_SANS_32_WHITE).then(font => ([tpl, font]))
+    ))
+    .then(data => {
+      tpl = data[0];
+      font = data[1];
+      return tpl.print(font, textData.placementX, textData.placementY, {
+        text: textData.text,
+        alignmentX: Jimp.HORIZONTAL_ALIGN_CENTER,
+        alignmentY: Jimp.VERTICAL_ALIGN_MIDDLE
+      }, textData.maxWidth, textData.maxHeight);
+    })
+    .then(tpl => (tpl.quality(100).write(imgExported)))
+    .then(tpl => {
+      console.log('exported file: ' + imgExported);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  res.send("Check");
+})
 
-// app.use(express.static("public"))
+app.use(express.static("public"))
 app.listen(port);
 console.log("App listening on port " + port);
