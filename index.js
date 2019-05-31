bd251a03e23cd4d8931f4733823a8ba1685f8fe9
sushant <paudelsushant482@gmail.com>
2019-05-31 10:10:30 +0545

    New edit on scaling

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var port = process.env.PORT || 8080;
 var path = require('path');
 var watermark = require('image-watermark');
 var Jimp = require('jimp');
-var sizeOf = require('image-size');
+// var sizeOf = require('image-size');
 
 
 
@@ -33,41 +33,39 @@ let imgExported = './public/src/image1.jpg';
 
 
 app.get('/watermark', function (req, res) {
-  sizeOf(imgRaw, function (err, dimensions) {
-    console.log(dimensions.width, dimensions.height);
-    Jimp.read(imgRaw)
-      .then(tpl =>
-        Jimp.read(imgLogo).then(logoTpl => {
-          logoTpl.opacity(0.4);
-          // console.log(dimensions.width, dimensions.height);
-          const height=dimensions.height;
-          const width=dimensions.width;
-          // console.log(height/2,width/2);
-          // console.log(tpl.getHeight(),tpl.getWidth());
-          const l=tpl.getHeight();
-          const b=tpl.getWidth();
-          logoTpl.resize(2*(width/3),2*(height/3));
-          console.log(logoTpl.getWidth(),logoTpl.getHeight())
-          // const x=logoTpl.getHeight();
-          // const y=logoTpl.getWidth();
-          return tpl.composite(logoTpl,
-            width/6,
-            height/6,
-            // (dimensions.height) / 2, //Height from Top
-            // (dimensions.width) / 2, //Width from left corner
-            Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
-        }))
-      .then(tpl => (tpl.quality(100).write(imgExported)))
-      .then(tpl => {
-        console.log('exported file: ' + imgExported);
-      })
-      .catch(err => {
-        console.error(err);
-      });
-    res.send("Check");
-  });
-})
+  Jimp.read(imgRaw)
+    .then(tpl =>
+      Jimp.read(imgLogo).then(logoTpl => {
+        logoTpl.opacity(0.4);
+        // console.log(dimensions.width, dimensions.height);
+        const height = tpl.getHeight();
+        const width = tpl.getWidth();
+        // console.log(height/2,width/2);
+        // console.log(tpl.getHeight(),tpl.getWidth());
+        const watermarkHeight = logoTpl.getHeight();
+        const watermarkWidth = logoTpl.getWidth();
 
+        const newHeight = (2.5 / 3) * height;
+        const newWidth = newHeight / watermarkHeight * watermarkWidth;
+
+        logoTpl.resize(newWidth, newHeight);
+
+        return tpl.composite(logoTpl,
+          (width - newWidth) / 2,
+          (height - newHeight) / 2,
+          // (dimensions.height) / 2, //Height from Top
+          // (dimensions.width) / 2, //Width from left corner
+          Jimp.HORIZONTAL_ALIGN_CENTER | Jimp.VERTICAL_ALIGN_MIDDLE);
+      }))
+    .then(tpl => (tpl.quality(100).write(imgExported)))
+    .then(tpl => {
+      console.log('exported file: ' + imgExported);
+    })
+    .catch(err => {
+      console.error(err);
+    });
+  res.send("Check");
+});
 app.use(express.static("public"))
 app.listen(port);
 console.log("App listening on port " + port);
